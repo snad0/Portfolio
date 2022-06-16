20a32cc4b0504c2649e62aebc13eb6c2bd6679fb
Mohd nadeem <nadeemsaifi497@gmail.com>
2022-06-16 11:23:39 +0530

    initial

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+  
+
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return render_template('index.html')
+
+
+# @app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(debug=True)
